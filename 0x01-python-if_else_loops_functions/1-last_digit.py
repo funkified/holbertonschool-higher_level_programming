4374e87d3bb154ca9f62f5a01a5692c4db0ad0e2
Sergio Vera <sergio.vera@icloud.com>
2021-09-08 16:37:15 +0000

    fixing else loop

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -12,5 +12,5 @@ if last_digit > 5:
     print("{} and is greater than 5".format(last_digit))
 if last_digit == 0:
     print("{} and is zero".format(last_digit))
-if last_digit < 6 and last_digit != 0:
+if number < 6 and last_digit != 0:
     print("{} and is less than 6 and not 0".format(last_digit))
