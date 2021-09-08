62f9d1b6a6a6b8deafeeaa34356eac1ffc363a16
Sergio Vera <sergio.vera@icloud.com>
2021-09-08 19:37:56 +0000

    removing extra print

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -5,5 +5,4 @@ for i in range(0, 10):
         if j > i:
             print("{:d}{:d}, ".format(i, j), end=" ")
         if i == 8 and j == 9:
-            print("{:d}{:d}".format(i, j), end=" ")
-print(" ")
+            print("{:d}{:d}".format(i, j))
