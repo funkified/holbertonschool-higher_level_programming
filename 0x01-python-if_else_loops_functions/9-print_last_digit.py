c8bb3f048ad755ab58e3d6d99e748cc9ad5f40d9
Sergio Vera <sergio.vera@icloud.com>
2021-09-08 23:26:37 +0000

    removing loop

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    last_digit = number % 10
-    if number >= 0:
-        print("{}".format(last_digit), end="")
-    else:
-        last_digit = number % -10
-        print(abs(last_digit), end="")
+    last_digit = abs(number) % 10
+    print("{}".format(last_digit), end="")
     return abs(last_digit)
 
