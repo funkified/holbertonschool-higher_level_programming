2678d0e4400d35c4d6ce05ddc49d926cc95344ae
Sergio Vera <sergio.vera@icloud.com>
2021-09-09 00:27:58 +0000

    fizzbuzz

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -3,7 +3,7 @@ def fizzbuzz():
     for i in range(1, 100 + 1):
         if i % 3 != 0 and i % 5 != 0:
             print(i, end=' ')
-        if i % 3 == 0 and i % 5 != 0:
+        if i % 3 == 0:
             print("Fizz", end=' ')
         if i % 5 == 0:
             print("Buzz", end=' ')
