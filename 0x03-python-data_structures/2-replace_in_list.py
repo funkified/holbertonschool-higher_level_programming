920fd12ac51502d42fba01a2f38318a435c3850d
Sergio Vera <sergio.vera@icloud.com>
2021-09-14 18:03:01 +0000

    using enumerate in for loop

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    for idx in my_list:
+    for idx, element in enumerate(my_list):
         if idx < 0 or idx > len(my_list) - 1:
             return my_list
-        else:
-            my_list[idx + 2] = element
+        if my_list[idx] == 3:
+            my_list[element] = 9
             return my_list
