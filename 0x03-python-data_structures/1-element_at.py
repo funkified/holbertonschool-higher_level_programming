eb9e89b3827f653bd81f118e6ac7082a0f7fcf53
Sergio Vera <sergio.vera@icloud.com>
2021-09-14 16:40:44 +0000

    removing for loop

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    for idx in my_list:
-        if idx < 0:
-            return None
-        if idx > my_list[idx]:
-            return None
-        else:
-            return my_list[3]
+    if idx < 0:
+        return None
+    if idx > len(my_list) - 1:
+        return None
+    else:
+        return my_list[idx]
