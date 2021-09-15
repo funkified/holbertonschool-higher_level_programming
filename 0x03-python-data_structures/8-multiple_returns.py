f4cae2db137ab2960fdcde3058bf68ad2592091a
Sergio Vera <sergio.vera@icloud.com>
2021-09-15 01:28:24 +0000

    if sentence empty

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     size = len(sentence)
-    first = sentence[0]
-    if size > 0:
-        return (size, first)
-    else:
-        None
+    return (size, sentence[0] if size > 0 else None)
