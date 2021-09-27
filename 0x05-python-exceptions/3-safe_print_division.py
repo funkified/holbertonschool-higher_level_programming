45a0f4a5f08c2864091776165d7e5b078adc903f
Sergio Vera <sergio.vera@icloud.com>
2021-09-27 20:44:15 +0000

    return NOne

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -5,6 +5,6 @@ def safe_print_division(a, b):
         res = a / b
         return res
     except ZeroDivisionError:
-        return None
+        return res
     finally:
         print("Inside result: {}". format(res))
