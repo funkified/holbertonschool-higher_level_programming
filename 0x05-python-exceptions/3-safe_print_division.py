ec800b2397ac43af7cb9ad87b25a40e22e2d4bca
Sergio Vera <sergio.vera@icloud.com>
2021-09-27 20:37:29 +0000

    removing try

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
+    res = None
     try:
-        res = None
-        try:
-            res = a / b
-            return res
-        except ZeroDivisionError:
+        res = a / b
+        return res
+    except ZeroDivisionError:
             return None
     finally:
         print("Inside result: {}". format(res))
