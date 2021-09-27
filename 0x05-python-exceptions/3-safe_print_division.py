78509cee6f5d1266a7aec7d95487cdc1ba764546
Sergio Vera <sergio.vera@icloud.com>
2021-09-27 20:56:52 +0000

    removing return , setting result to None

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
-    res = None
     try:
         res = a / b
-        return res
     except ZeroDivisionError:
-        return res
+        res = None
     finally:
         print("Inside result: {}". format(res))
+    return res
