c57d54817797a94cf05c9516c59388ca70b6cda5
Sergio Vera <sergio.vera@icloud.com>
2021-09-28 19:19:35 +0000

    tasl 0 - 3

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -2,6 +2,8 @@
 """ 0-square.py
 This module contains an empty class
 """
+
+
 class Square:
     """ Represents an empty class call Square """
     pass
---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" 2-square.py
+Defines a square by 1-square.py
+"""
+
+
+class Square:
+    """
+    This is a class Square that define a square
+    
+    Attributes:
+    __size: the int size of square
+    """
+
+    def __init__(self, size=0):
+        """
+        Method __init__ to initialize class
+        Parameter:
+            size: size of squuare
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be an integer")
+        else:
+            self.__size = size
