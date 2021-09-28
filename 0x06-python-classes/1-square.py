8a668921ebe58077606b30aa98cd56f47d836869
Sergio Vera <sergio.vera@icloud.com>
2021-09-28 16:36:52 +0000

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" 1-square.py
+Defines a square by size
+"""
+
+
+class Square:
+    """ Instance of private atributte __size
+    """
+    def __init__(self, size):
+        self.__size = size
