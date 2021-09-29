cbdfd1a24d65cbdb5008b193913c22ac8cd6cf2b
Sergio Vera <sergio.vera@icloud.com>
2021-09-29 01:06:11 +0000

    adding condition for exception

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -47,7 +47,7 @@ class Square:
                     raise TypeError("position must be a tuple \
                             of 2 positive integers")
         else:
-            self.__position = position
+            self.__position = value
 
     def area(self):
         """
