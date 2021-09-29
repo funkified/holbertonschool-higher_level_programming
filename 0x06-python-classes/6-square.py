e77c11f380135fdf18b819a459c038485f00ea53
Sergio Vera <sergio.vera@icloud.com>
2021-09-29 01:02:06 +0000

    adding condition for exception

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -41,8 +41,11 @@ class Square:
 
     @position.setter
     def position(self, value):
-        if type(value) is not tuple or len(value) is not 2:
-            raise TypeError("position must be a tuple of 2 positive integers")
+        if type(value) is not tuple or \
+                len(value) is not 2 and \
+                type(value) is not int:
+                    raise TypeError("position must be a tuple \
+                            of 2 positive integers")
         else:
             self.__position = position
 
