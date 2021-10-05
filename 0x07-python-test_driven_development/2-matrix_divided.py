81e12382831bc6da9aabfd957431f6dd3de7d72c
Sergio Vera <sergio.vera@icloud.com>
2021-10-05 22:50:36 +0000

    adding more test task 4

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -2,12 +2,15 @@
 """
 2-matrix_divided
 """
+
+
 def matrix_divided(matrix, div):
+    """ Divide a Matrix by two """
     if type(matrix) != list:
         raise TypeError("matrix must be a matrix (list of lists) of\
                 integers/floats")
+    size = None
     for row in matrix:
-        size = None
         if type(row) != list:
             raise TypeError("matrix must be a matrix (list of lists) of\
                     integers/floats")
@@ -15,8 +18,8 @@ def matrix_divided(matrix, div):
             size = len(row)
         elif size != len(row):
             raise TypeError("Each row of the matrix must have the same size")
-        if type(div) not in [int, float]:
+    if type(div) not in [int, float]:
             raise TypeError("div must be a number")
-        if div == 0:
+    if div == 0:
             raise ZeroDivisionError("division by zero")
-        return [[round(element / div, 2) for element in row] for row in matrix]
+    return [[round(element / div, 2) for element in row] for row in matrix]
--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 """
+Print Square
 """
 
+
 def print_square(size):
-        if type(size) is not int:
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
-        for i in range(size):
+    """ Function to print a square and handling errors"""
+    if type(size) is not int:
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    for i in range(size):
             print("#" * size)
