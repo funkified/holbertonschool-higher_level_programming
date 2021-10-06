6e40dbf8d71eabeb6ca75196808497f77ae1d245
Sergio Vera <sergio.vera@icloud.com>
2021-10-06 19:19:03 +0000

    adding cases 5

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
+"""
+Task 5
+"""
+
+
 def text_indentation(text):
     """
     Method to print a string
     Text: string to print
         if is not a string raise exception
     """
-    if type(text) != str:
+    if type(text) is not str:
         raise TypeError("text must be a string")
 
     text = text.strip()
