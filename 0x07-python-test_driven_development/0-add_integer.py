3955142f9367bab21146cf93cfe1d3765625d761
Sergio Vera <sergio.vera@icloud.com>
2021-10-06 03:10:41 +0000

    pep8 task 0

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -4,9 +4,11 @@ Task: 0-add_integer
 Test: Cases for unittest
 Args: a+b
 """
+
+
 def add_integer(a, b=98):
     """
-    Add integers:    a + b 
+    Add integers:    a + b
     """
     if type(a) not in [int, float]:
         raise TypeError("a must be an integer")
