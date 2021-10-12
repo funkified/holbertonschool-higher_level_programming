01dfaaf893afa7f99106803cd48fdc6d2168d289
Sergio Vera <sergio.vera@icloud.com>
2021-10-12 19:39:01 +0000

    task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+TAsk 6
+"""
+
+
+class BaseGeometry:
+    """
+    BaseGeometry
+    """
+    def area(self):
+        """ raise Exception """
+        raise Exception("area() is not implemented")
