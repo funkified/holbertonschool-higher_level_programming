76b7c8a1a119762aba057046d5caf4d19b678df9
Sergio Vera <sergio.vera@icloud.com>
2021-10-12 15:31:02 +0000

    task 0

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Task 0: Lookup
+"""
+
+
+def lookup(obj):
+    """
+    Function that returns the list of avaialble attributes
+    and methors of an object
+    """
+    return dir(obj)
