245b7b37704c71ab707b7598dc1526331b504d05
Sergio Vera <sergio.vera@icloud.com>
2021-10-13 22:43:59 +0000

    task 1 fix

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,7 +3,8 @@
 task1
 """
 
+
 def read_file(filename=""):
     """read a file"""
-    with open("my_file_0.txt", mode="r", encoding="utf8") as f:
+    with open(filename, 'r', encoding="utf8") as f:
         print(f.read(), end="")
