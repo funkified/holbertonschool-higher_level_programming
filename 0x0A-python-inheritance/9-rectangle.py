2b5b9582b45e3cf94e85cb48fafc8b15ca3f81fb
Sergio Vera <sergio.vera@icloud.com>
2021-10-13 02:43:39 +0000

    docstring

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -21,4 +21,5 @@ class Rectangle(BaseGeometry):
         return self.__width * self.__height
 
     def __str__(self):
+        """ string """
         return "[Rectangle] {:d}/{:d}".format(self.__width, self.__height)
