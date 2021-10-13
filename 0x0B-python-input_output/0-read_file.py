65c244fa4184a60d85c51f7eb57ae9b5f9786695
Sergio Vera <sergio.vera@icloud.com>
2021-10-13 22:48:58 +0000

    task 0 fix

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """
-task1
+Mwethod that reads the content of a text file
 """
 
 
 def read_file(filename=""):
     """read a file"""
-    with open(filename, 'r', encoding="utf8") as f:
+    with open(filename, 'r', encoding='UTF-8') as f:
         print(f.read(), end="")
