9cba79513d0f851128df74501ee4d7ada814edcf
Sergio Vera <sergio.vera@icloud.com>
2021-10-20 18:52:36 +0000

    test_base

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -3,16 +3,27 @@
 Module: Unittest for models
 """
 
+from tests import *
 import unittest
-import inspect
-import pep8
-from models import base
-Base = base.Base
+from models.base import Base
+
 
 class testBase(unittest.TestCase):
     """Testing for class Base"""
-    def test_args(self):
-        """ test if there is too many arguments"""
-        with self.assertRaises(TypeError):
-            b = Base(1, 1)
 
+    @classmethod
+    def setUpClass(self):
+        self.a_base = Base()
+        self.b_base = Base()
+        self.c_base = Base(89)
+
+    def test_init(self):
+        self.assertEqual(self.a_base.id, 1)
+
+    def test_assign_id(self):
+        self.assertEqual(self.c_base.id, 89)
+
+"""
+    if __name__ == "__main__":
+        unittest.main()
+"""
