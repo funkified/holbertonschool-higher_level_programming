f3cf204456fdada712f7f3409217f375fde2d24a
Sergio Vera <sergio.vera@icloud.com>
2021-10-20 14:31:10 +0000

    json string

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -3,6 +3,8 @@
 Class Base
 """
 
+import json
+import csv
 
 class Base:
     """Class"""
@@ -15,3 +17,12 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = self.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """
+        return json string representation of list_dictionaries
+        """
+        if list_dictionaries is None:
+            return []
+        return json.dumps(list_dictionaries)
