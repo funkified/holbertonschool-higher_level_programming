2b29b39f8698ebc4fe1ed5ab81034b280cd5c64a
Sergio Vera <sergio.vera@icloud.com>
2021-11-17 01:25:47 +0000

    fixing code

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,7 +1,4 @@
 -- A script that creates a table called:
 -- first_table, id INT, name VARCHAR
 CREATE IF NOT EXISTS first_table
-(
-    id INT,
-    name VARCHAR(256)
-);
+(id INT, name VARCHAR(256));
