222f589d97a77103def404218fd94bc87791e609
Sergio Vera <sergio.vera@icloud.com>
2021-11-17 02:10:22 +0000

    fixing code

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,5 +1,4 @@
 -- List all record from second table
 -- select the rows greater than 10 in descending order
-SELECT name, score FROM second_table
-WHERE score >= 10
+SELECT name, score FROM second_table WHERE score >= 10
 ORDER BY score DESC;
