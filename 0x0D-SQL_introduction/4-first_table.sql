9d2c90eb77bc486d16b757cab66a58e5ef2ed87a
Sergio Vera <sergio.vera@icloud.com>
2021-11-17 00:00:06 +0000

    fixing syntax

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,3 +1,5 @@
 -- A script that creates a table called:
 -- first_table, id INT, name VARCHAR
-CREATE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
+CREATE IF NOT EXISTS first_table(
+    id INT,
+    name VARCHAR(256));
