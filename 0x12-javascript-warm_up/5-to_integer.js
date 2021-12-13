a548bfefc1e2d6c38d99aaa2adbf992e7483038a
Sergio Vera <sergio.vera@icloud.com>
2021-12-13 22:42:12 +0000

    fixing code

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
-
 const firstArg = process.argv[2];
 
 if (isNaN(parseInt(firstArg))) {
     console.log('Not a number');
 } else {
-    console.log('My number: ' + parseInt(firstArg));
+    console.log('My number: ' + firstArg);
 }
