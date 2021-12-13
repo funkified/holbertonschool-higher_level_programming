ed4c694bfb2660091f8efa7500e5854bc649384b
Sergio Vera <sergio.vera@icloud.com>
2021-12-13 22:44:43 +0000

    fixin indent

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,7 +2,7 @@
 const firstArg = process.argv[2];
 
 if (isNaN(parseInt(firstArg))) {
-    console.log('Not a number');
+   console.log('Not a number');
 } else {
-    console.log('My number: ' + firstArg);
+  console.log('My number: ' + firstArg);
 }
