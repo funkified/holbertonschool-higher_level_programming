e80838e78e1b16ddd1c8a4543dc4922f57faceef
Sergio Vera <sergio.vera@icloud.com>
2021-12-14 00:59:15 +0000

    task 6

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
-const arr = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const arr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 for (const i of arr) {
-    console.log(i);
+  console.log(i);
 }
