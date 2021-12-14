50c85c66ecf091ff763dfe94683baf09ccbbf2d2
Sergio Vera <sergio.vera@icloud.com>
2021-12-14 02:49:45 +0000

    task 10

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -2,8 +2,8 @@
 const n = parseInt(process.argv[2]);
 function factorial (n) {
   if (isNaN(n) || n <= 1) {
-      return 1;
+    return 1;
   }
-    return n * factorial(n -1);
+  return n * factorial(n -1);
 }
 console.log(factorial(n));
