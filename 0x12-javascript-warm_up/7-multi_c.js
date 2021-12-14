31119faecd5fb156b4150021e5e3738cc5d423ec
Sergio Vera <sergio.vera@icloud.com>
2021-12-14 02:11:26 +0000

    task 7

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,10 +1,7 @@
 #!/usr/bin/node
-
 const str = parseInt(process.argv[2]);
 if (isNaN(str)) {
   console.log('Missing number of occurrences');
 } else {
-  for (let i = 0; i < str; i++) {
-      console.log('C is fun');
-  }
+  for (let i = 0; i < str; i++) { console.log('C is fun'); }
 }
