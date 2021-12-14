1d849184fe62766b491622e326b1a862861d0aac
Sergio Vera <sergio.vera@icloud.com>
2021-12-14 03:50:16 +0000

    fix

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -4,9 +4,9 @@ const myObject = {
   value: 12
 };
 console.log(myObject);
-myObject.incr = function () {
-  this.value++;
-};
+
+myObject.incr = function () { myObject.value += 1; };
+
 myObject.incr();
 console.log(myObject);
 myObject.incr();
