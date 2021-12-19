b4d76970b8b15dcad391caa502ba974d6575409a
Sergio Vera <sergio.vera@icloud.com>
2021-12-19 23:50:08 +0000

    adding session.configure

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -10,7 +10,7 @@ engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.format(
     argv[1], argv[2], 'localhost', argv[3]))
 # Base.metadata.create_all(engine)
 Session = sessionmaker(bind=engine)
-# Session.configure(bind=engine)
+Session.configure(bind=engine)
 session = Session()
 
 session_query = session.query(State).order_by(State.id)
