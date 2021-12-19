48726f864d34b7341d2837432dddaad1c8ec44f6
Sergio Vera <sergio.vera@icloud.com>
2021-12-19 22:29:03 +0000

    fixing code

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -13,11 +13,16 @@ if __name__ == '__main__':
                            user=argv[1],
                            passwd=argv[2],
                            db=argv[3])
+
     cursor = conn.cursor()
-    cursor.execute("SELECT * FROM states.name\
-                    ORDER BY cities.id ASC")
+    cursor.execute("SELECT cities.name FROM cities\
+    JOIN states ON cities.states_id = states.id WHERE states.name=%s\
+    ORDER BY cities.id ASC", (argv[4],))
+
     rows = cursor.fetchall()
-    for states in rows:
-        print("{}".format(states))
+    for city in rows:
+        city += rows
+    print(", ".join(city))
+
     cursor.close()
     conn.close()
