1bd06845b1336fa6bc6c045e9b4eba0e61836137
Sergio Vera <sergio.vera@icloud.com>
2021-12-19 23:53:39 +0000

    adding session.configure

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
-
+"""
+Connecting script with my database
+"""
+from model_state import State, Base
+from sys import argv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
 import sqlalchemy
-from sys import argv
 
 engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.format(
     argv[1], argv[2], 'localhost', argv[3]))
 # Base.metadata.create_all(engine)
-Session = sessionmaker(bind=engine)
-Session.configure(bind=engine)
+Session = sessionmaker(engine)
+# Session.configure(bind=engine)
 session = Session()
 
 session_query = session.query(State).order_by(State.id)
