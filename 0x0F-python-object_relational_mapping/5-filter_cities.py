78f571a5ed755e2ee0009947c27bc3d44f7b2cdc
Sergio Vera <sergio.vera@icloud.com>
2021-12-19 22:40:05 +0000

    fixing code

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -7,22 +7,16 @@ import MySQLdb
 from sys import argv
 
 
-if __name__ == '__main__':
-    conn = MySQLdb.connect(host="localhost",
-                           port=3306,
-                           user=argv[1],
-                           passwd=argv[2],
-                           db=argv[3])
-
-    cursor = conn.cursor()
-    cursor.execute("SELECT cities.name FROM cities\
-    JOIN states ON cities.states_id = states.id WHERE states.name=%s\
+if __name__ == "__main__":
+    dbConnect = MySQLdb.connect(host="localhost",
+                                port=3306,
+                                user=argv[1],
+                                passwd=argv[2],
+                                db=argv[3])
+    cursor = dbConnect.cursor()
+    cursor.execute("SELECT cities.name FROM cities \
+    JOIN states ON cities.state_id = states.id WHERE states.name=%s \
     ORDER BY cities.id ASC", (argv[4],))
-
-    rows = cursor.fetchall()
-    for city in rows:
-        city += rows
-    print(", ".join(city))
-
+    print(", ".join(city[0] for city in cursor.fetchall()))
     cursor.close()
-    conn.close()
+    dbConnect.close()
