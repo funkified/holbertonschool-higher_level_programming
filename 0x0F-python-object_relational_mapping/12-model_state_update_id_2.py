7945447b0642f09f9b8f462c110fc82f06f5de95
Sergio Vera <sergio.vera@icloud.com>
2021-12-20 02:50:15 +0000

    task 12

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Connecting script with my database
+"""
+from model_state import State, Base
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import sqlalchemy
+
+if __name__ == '__main__':
+
+    engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.format(
+        argv[1], argv[2], 'localhost', argv[3]))
+
+    Session = sessionmaker(engine)
+    session = Session()
+
+    query = session.query(State).filter(State.id == 2).update(
+            {State.name: 'New Mexico'})
+
+    session.commit()
+    session.close()
