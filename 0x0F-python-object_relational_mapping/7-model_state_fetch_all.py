e80e65581e66d37a29e342a88ab22878ff602c10
Sergio Vera <sergio.vera@icloud.com>
2021-12-20 01:19:28 +0000

    adding if__name__==__main__: statement

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -2,20 +2,23 @@
 """
 Connecting script with my database
 """
+
+
 from model_state import State, Base
 from sys import argv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 import sqlalchemy
 
-engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.format(
-    argv[1], argv[2], 'localhost', argv[3]))
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.format(
+        argv[1], argv[2], 'localhost', argv[3]))
 # Base.metadata.create_all(engine)
-Session = sessionmaker(engine)
-# Session.configure(bind=engine)
-session = Session()
+    Session = sessionmaker(bind=engine)
+    Session.configure(bind=engine)
+    session = Session()
 
-session_query = session.query(State).order_by(State.id)
-for state in session_query:
-    print("{}: {}".format(state.id, state.name))
-session.close()
+    session_query = session.query(State).order_by(State.id)
+    for state in session_query:
+        print("{}: {}".format(state.id, state.name))
+    session.close()
