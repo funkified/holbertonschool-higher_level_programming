4c66d62b651fbb3a3f4d43fe16220ce56a74fd24
Sergio Vera <sergio.vera@icloud.com>
2021-12-20 01:23:23 +0000

    adding if__name__==__main__: statement

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -8,16 +8,17 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 import sqlalchemy
 
-engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.format(
-    argv[1], argv[2], 'localhost', argv[3]))
-# Base.metadata.create_all(engine)
-Session = sessionmaker(engine)
-# Session.configure(bind=engine)
-session = Session()
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.format(
+        argv[1], argv[2], 'localhost', argv[3]))
+    # Base.metadata.create_all(engine)
+    Session = sessionmaker(engine)
+    # Session.configure(bind=engine)
+    session = Session()
 
-state = session.query(State).order_by(State.id).first()
-if state:
-    print("{}: {}".format(state.id, state.name))
-else:
-    print("Nothing")
-session.close()
+    state = session.query(State).order_by(State.id).first()
+    if state:
+        print("{}: {}".format(state.id, state.name))
+    else:
+        print("Nothing")
+    session.close()
