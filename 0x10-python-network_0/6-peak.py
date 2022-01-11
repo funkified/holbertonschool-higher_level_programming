ad2ddec6e2d4014c4e502bfbf7d8445afe91b901
Sergio Vera <sergio.vera@icloud.com>
2022-01-11 19:02:13 +0000

    fixinx code

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -13,19 +13,19 @@ def find_peak(list_of_integers):
         hi = lenght
         lo = 0
         mid = lenght // 2
-        if lenght == 1:
-            return list_of_integers[1]
-        if mid == hi:
-            return list_of_integers[mid]
-        if mid - 1 > mid:
-            hi = mid - 1
-            return list_of_integers[hi]
+#    if lenght == 1:
+#        return list_of_integers[1]
+#    if mid == hi:
+#        return list_of_integers[mid]
+#    if mid - 1 > mid:
+#        hi = mid - 1
+#        return list_of_integers[hi]
         if list_of_integers[lo] > list_of_integers[1]:
             return list_of_integers[0]
         if list_of_integers[-1] > list_of_integers[-2]:
             return list_of_integers[-1]
 
-    for idx in range(0, len(list_of_integers)):
+    for idx in range(1, len(list_of_integers)):
         if (list_of_integers[idx - 1] < list_of_integers[idx] and
                 list_of_integers[idx] > list_of_integers[idx + 1]):
             return list_of_integers[idx]
