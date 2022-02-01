dee04af29a785f9e1bb46ebba010877d93276ea3
Sergio Vera <sergio.vera@icloud.com>
2022-02-01 16:53:54 +0000

    fix

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
-const fs = require('fs')
+const fs = require('fs');
 
-fs.readFile(process.argv[2], 'utf-8', (err, data) => {
-	if (err) {
-		console.log(err);
-	} else {
-	console.log(data);
-	}
-})
+fs.readFile(process.argv[2], 'utf-8', function (err, data) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
+});
